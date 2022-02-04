bd967804fed6bb09610179d4ce5acc9793c041e7
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-02-03 21:30:41 -0600

    Group by errors / no errors

--- src/04-TypeClasses/DifferentMonads.purs
+++ src/04-TypeClasses/DifferentMonads.purs
@@ -31,57 +31,65 @@ program = do
 main :: Effect Unit
 main = do
   log "=== Identity ==="
+  log "No Errors"
   log
     $ "ExceptTStateT - Identity - initial state of 1: "
     <> show (runExceptT (runStateT program 1) :: Identity (Either String (Tuple Boolean Int)))
   log
-    $ "ExceptTStateT - Identity - initial state of 5: "
-    <> show (runExceptT (runStateT program 5) :: Identity (Either String (Tuple Boolean Int)))
-  log
     $ "StateTExceptT - Identity - initial state of 1: "
     <> show (runStateT (runExceptT program) 1 :: Identity (Tuple (Either String Boolean) Int))
+  log "\nErrors"
+  log
+    $ "ExceptTStateT - Identity - initial state of 5: "
+    <> show (runExceptT (runStateT program 5) :: Identity (Either String (Tuple Boolean Int)))
   log
     $ "StateTExceptT - Identity - initial state of 5: "
     <> show (runStateT (runExceptT program) 5 :: Identity (Tuple (Either String Boolean) Int))
 
   log "\n\n=== Maybe ==="
+  log "No Errors"
   log
     $ "ExceptTStateT - Maybe - initial state of 1: "
     <> show (runExceptT (runStateT program 1) :: Maybe (Either String (Tuple Boolean Int)))
   log
-    $ "ExceptTStateT - Maybe - initial state of 5: "
-    <> show (runExceptT (runStateT program 5) :: Maybe (Either String (Tuple Boolean Int)))
-  log
     $ "StateTExceptT - Maybe - initial state of 1: "
     <> show (runStateT (runExceptT program) 1 :: Maybe (Tuple (Either String Boolean) Int))
+  log "\nErrors"
+  log
+    $ "ExceptTStateT - Maybe - initial state of 5: "
+    <> show (runExceptT (runStateT program 5) :: Maybe (Either String (Tuple Boolean Int)))
   log
     $ "StateTExceptT - Maybe - initial state of 5: "
     <> show (runStateT (runExceptT program) 5 :: Maybe (Tuple (Either String Boolean) Int))
 
   log "\n\n=== Either Boolean ==="
+  log "No Errors"
   log
     $ "ExceptTStateT - Either Boolean - initial state of 1: "
     <> show (runExceptT (runStateT program 1) :: Either Boolean (Either String (Tuple Boolean Int)))
   log
-    $ "ExceptTStateT - Either Boolean - initial state of 5: "
-    <> show (runExceptT (runStateT program 5) :: Either Boolean (Either String (Tuple Boolean Int)))
-  log
     $ "StateTExceptT - Either Boolean - initial state of 1: "
     <> show (runStateT (runExceptT program) 1 :: Either Boolean (Tuple (Either String Boolean) Int))
+  log "\nErrors"
+  log
+    $ "ExceptTStateT - Either Boolean - initial state of 5: "
+    <> show (runExceptT (runStateT program 5) :: Either Boolean (Either String (Tuple Boolean Int)))
   log
     $ "StateTExceptT - Either Boolean - initial state of 5: "
     <> show (runStateT (runExceptT program) 5 :: Either Boolean (Tuple (Either String Boolean) Int))
 
   log "\n\n=== Array ==="
+  log "No Errors"
   log
     $ "ExceptTStateT - Array - initial state of 1: "
     <> show (runExceptT (runStateT program 1) :: Array (Either String (Tuple Boolean Int)))
   log
-    $ "ExceptTStateT - Array - initial state of 5: "
-    <> show (runExceptT (runStateT program 5) :: Array (Either String (Tuple Boolean Int)))
-  log
     $ "StateTExceptT - Array - initial state of 1: "
     <> show (runStateT (runExceptT program) 1 :: Array (Tuple (Either String Boolean) Int))
+  log "\nErrors"
+  log
+    $ "ExceptTStateT - Array - initial state of 5: "
+    <> show (runExceptT (runStateT program 5) :: Array (Either String (Tuple Boolean Int)))
   log
     $ "StateTExceptT - Array - initial state of 5: "
     <> show (runStateT (runExceptT program) 5 :: Array (Tuple (Either String Boolean) Int))
@@ -95,11 +103,13 @@ main = do
   -- log $ "Implementation - Effect - initial state of X: " <> show result
   -- ```
   log "\n\n=== Effect ==="
+  log "No Errors"
   join $ map (log <<< append "ExceptTStateT - Effect - initial state of 1: " <<< show) do
     runExceptT (runStateT program 1)
-  join $ map (log <<< append "ExceptTStateT - Effect - initial state of 5: " <<< show) do
-    runExceptT (runStateT program 5)
   join $ map (log <<< append "StateTExceptT - Effect - initial state of 1: " <<< show) do
     runStateT (runExceptT program) 1
+  log "\nErrors"
+  join $ map (log <<< append "ExceptTStateT - Effect - initial state of 5: " <<< show) do
+    runExceptT (runStateT program 5)
   join $ map (log <<< append "StateTExceptT - Effect - initial state of 5: " <<< show) do
     runStateT (runExceptT program) 5
