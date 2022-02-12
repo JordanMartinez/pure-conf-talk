50494ae6230eee0a56a63d98fa18b04a099078c1
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-02-11 20:13:02 -0600

    Make difference more visible

--- src/05-TypeClasses/StackOrder.purs
+++ src/05-TypeClasses/StackOrder.purs
@@ -19,32 +19,32 @@ program
 program = do
   initialState <- get
   unless (initialState < 3) do
-    throwError $ "Initial state '" <> show initialState <> "' must be less than 3!"
+    throwError $ "Initial state must be less than 3!"
   put 0
   pure true
 
 -- Note: the first transformer newtype we unwrap is the OUTERMOST transformer.
 main :: Effect Unit
 main = do
-  log "No Errors"
+  log "No Errors (happy path)"
   log
-    $ "ExceptTStateT - happy path: "
+    $ "ExceptTStateT: "
         -- The type annotation here is only needed because the
         -- monadic type is not specified. Is it `Identity`? `Either`? `Array`?
         -- The compiler has no way to know.
         <> show (runExceptT (runStateT program 1) :: Identity _)
   log
-    $ "StateTExceptT - happy path: "
+    $ "StateTExceptT: "
         <> show (runStateT (runExceptT program) 1 :: Identity _)
 
   log "\nErrors"
   log
-    $ "ExceptTStateT - error path: "
+    $ "ExceptTStateT: "
         -- `runExcept` is `runExceptT` with the `monadicType` specified to `Identity`
         -- So, no type annotation is needed here.
         <> show (runExcept (runStateT program 5))
   log
-    $ "StateTExceptT - error path: "
+    $ "StateTExceptT: "
         -- Each monad transformer has a `runNameT` and `runName` function
         -- Both unwrap the newtype. The latter indicates the monad is `Identity`.
         <> show (runState (runExceptT program) 5)
